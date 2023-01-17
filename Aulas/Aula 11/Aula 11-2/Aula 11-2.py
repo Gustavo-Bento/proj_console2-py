fbbe1de6a785681f83d7e4e82c2391163d048ea9
Gustavo Ferreira <gfmb94@gmail.com>
2023-01-17 11:23:48 -0300

    Exercícios de função map

---
+++ Aulas/Aula 11/Aula 11-2/Aula 11-2.py
@@ -0,0 +1,11 @@
+celsius = [7,10,12,17,20,24,30,35,40,50]
+lista = lambda c: round(c*1.8+32,2)
+fahrenheit = list(map(lista,celsius))
+lista = lambda c: round(c+273.15)
+kelvin = list(map(lista,celsius))
+
+print("{:<10}{:<12}{:<10}".format("\nCelsius","Fahrenheit","Kelvin"))
+print("===========================")
+
+for i in range(0,len(celsius),1):
+    print("{:<10}{:<12}{:<10}".format(celsius[i],fahrenheit[i],kelvin[i]))
